d733624031c6401a3f6a915af12e916877d04a31
cwj5012 <cwj5012@gmail.com>
2020-05-15 14:27:15 +0800

    1 cmake 模块取别名测试

--- src/hello_world/main.cpp
+++ src/hello_world/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "../rdm/submodule/SubClass.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    auto a = SubClass();
+    a.print();
     return 0;
 }
---
+++ src/rdm/submodule/SubClass.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+
+class SubClass {
+public:
+    void print() { std::cout << "SubClass.print()" << std::endl; }
+};
+
+
