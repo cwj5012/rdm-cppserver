7b8dabaee3da8d708fe5983c9003eef190e3a1e7
cwj5012 <cwj5012@gmail.com>
2020-05-25 19:38:00 +0800

    1

--- src/boost/main.cpp
+++ src/boost/main.cpp
@@ -4,17 +4,18 @@
 #include <boost/filesystem.hpp>
 
 int main(int argc, char* argv[]) {
-    std::cout << BOOST_VERSION << std::endl;
+    std::cout << "boost version: " << BOOST_VERSION << std::endl;
 
-    boost::filesystem::path dir_path("/home/me/test");
+    std::string path = "/home/me/test";
+    boost::filesystem::path dir_path(path);
 
     if (boost::filesystem::exists(dir_path))  //判断文件存在性
     {
-        std::cout << "directory exist." << std::endl;
+        std::cout << path <<" dir exist." << std::endl;
     } else {
-        std::cout << "directory not exist." << std::endl;
+        std::cout << path <<" dir not exist." << std::endl;
         boost::filesystem::create_directory(dir_path);
-        std::cout << "directory created." << std::endl;
+        std::cout << path <<" dir created." << std::endl;
     }
 
     return 0;
