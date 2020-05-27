73b6a57ac3954fcd0c01b2e6092ace57f6543199
cwj5012 <cwj5012@gmail.com>
2020-05-28 01:09:56 +0800

    1 测试 core dump 没有产生问题，clion 运行不能产生 core dump

--- src/hello_world/main.cpp
+++ src/hello_world/main.cpp
@@ -5,5 +5,10 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     auto a = SubClass();
     a.print();
+
+    // 测试产生 core dump
+    // int *p = NULL;
+    // *p = 0;
+
     return 0;
 }
