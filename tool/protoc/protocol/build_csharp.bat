918c4d1c0fc9854b3e7bc19244818701b50eb562
cwj5012 <cwj5012@gmail.com>
2020-05-29 10:26:27 +0800

    1

--- tool/protoc/protocol/build_csharp.bat
+++ tool/protoc/protocol/build_csharp.bat
@@ -2,6 +2,6 @@ echo off
 
 if not exist "out_csharp" mkdir out_csharp
 
-for %%i in (./proto/*.proto) do "./protoc.exe" ./proto/%%i --csharp_out=./out_csharp
+for %%i in (./proto/*.proto) do "../bin/protoc.exe" ./proto/%%i --csharp_out=./out_csharp
 
 pause
