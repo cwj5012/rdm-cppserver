a23799929815adb2c1745225d736f9b53d71a58d
cwj5012 <cwj5012@gmail.com>
2020-05-29 10:26:44 +0800

    1

--- src/boost/main.cpp
+++ src/boost/main.cpp
@@ -1,21 +1,26 @@
-#include <iostream>
-
-#include <boost/version.hpp>
-#include <boost/filesystem.hpp>
+#include "echo_client.h"
+#include "echo_server.h"
 
 int main(int argc, char* argv[]) {
-    std::cout << "boost version: " << BOOST_VERSION << std::endl;
-
-    std::string path = "/home/me/test";
-    boost::filesystem::path dir_path(path);
-
-    if (boost::filesystem::exists(dir_path))  //判断文件存在性
-    {
-        std::cout << path <<" dir exist." << std::endl;
+    if (argc >= 2) {
+        if (std::string(argv[1]) == "-c") {
+            if (argc != 4) {
+                std::cerr << "Usage: -c <host> <port>\n";
+                return 1;
+            } else {
+                echo_client(std::string(argv[2]), std::string(argv[3]));
+            }
+        } else if (std::string(argv[1]) == "-s") {
+            if (argc != 3) {
+                std::cerr << "Usage: -s <port>\n";
+                return 1;
+            } else {
+                echo_server(argv[2]);
+            }
+        }
     } else {
-        std::cout << path <<" dir not exist." << std::endl;
-        boost::filesystem::create_directory(dir_path);
-        std::cout << path <<" dir created." << std::endl;
+        std::cout << "run client: -c <host> <port>" << std::endl;
+        std::cout << "run server: -s <port>" << std::endl;
     }
 
     return 0;
