a5edd568620468a03a6b9efb6ff9bae7bc3ac871
cwj5012 <cwj5012@gmail.com>
2020-06-09 11:32:29 +0800

    1 add some leetcode solution

---
+++ src/leetcode/二叉树/遍历.cpp
@@ -0,0 +1,26 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        helper(root, result);
+        return result;
+    }
+
+    void helper(TreeNode* root, vector<int>& result) {
+        if (root == NULL) {
+            return;
+        }
+        result.push_back(root->val);
+        helper(root->left, result);
+        helper(root->right, result);
+    }
+};
