9ef1352514e7fd55e931665a0fae6ba89eab43b1
cwj5012 <80614569@qq.com>
2020-06-11 09:50:42 +0800

    1

--- src/hello_world/main.cpp
+++ src/hello_world/main.cpp
@@ -3,18 +3,30 @@
 
 #include <rdm/util/ExceptionHandle.h>
 
+const int* get() {
+    return new int(1);
+}
+
 int main() {
     // 初始化异常处理
-    rdm::ExceptionHandle::InitProcessExceptionHandler();
-    rdm::ExceptionHandle::InitThreadExceptionHandler();
-
-    std::cout << "Hello, World!" << std::endl;
-    auto a = SubClass();
-    a.print();
+    // rdm::ExceptionHandle::InitProcessExceptionHandler();
+    // rdm::ExceptionHandle::InitThreadExceptionHandler();
+    //
+    // std::cout << "Hello, World!" << std::endl;
+    // auto a = SubClass();
+    // a.print();
 
     // 测试产生 core dump
-    int *p = NULL;
-    *p = 0;
+    // int *p = NULL;
+    // *p = 0;
+
+    const int* const a = get();
+    const int* b = get();
+
+
+    std::cout << *a <<std::endl;
+    std::cout << *b <<std::endl;
+    delete a;
 
     return 0;
 }
